c59f46beb2bce4f62a26cbf0dd938e3741d176cb
jlhuang2 <jlhuang2@illinois.edu>
2018-11-24 20:49:58 -0600

    Updates

---
+++ precache-manifest.e84148e8ad64e84a7098389b75a78989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b8faf6e4d1d24b745ec",
+    "url": "/basket-ball/static/js/runtime~main.9b8faf6e.js"
+  },
+  {
+    "revision": "42a120abdb2d3001faae",
+    "url": "/basket-ball/static/js/main.42a120ab.chunk.js"
+  },
+  {
+    "revision": "0847291b7171ea00b860",
+    "url": "/basket-ball/static/js/1.0847291b.chunk.js"
+  },
+  {
+    "revision": "42a120abdb2d3001faae",
+    "url": "/basket-ball/static/css/main.87550553.chunk.css"
+  },
+  {
+    "revision": "0847291b7171ea00b860",
+    "url": "/basket-ball/static/css/1.6f6dc8b7.chunk.css"
+  },
+  {
+    "revision": "9c05fed849227e186472b1fc27048997",
+    "url": "/basket-ball/index.html"
+  }
+];
